e0027c16ababfc568b9618ae298ab4326dacd7de
Martim Bravo <martim@bravo.com.pt>
2019-12-03 10:46:26 +0000

    Word cloud component

--- year-of-spotify-charts/src/components/visualizations/WordCloud.jsx
+++ year-of-spotify-charts/src/components/visualizations/WordCloud.jsx
@@ -1,16 +1,77 @@
 import React, { Component } from 'react'
 
+const d3WordCloud = require('./d3/WordCloud')
+
+const MAX_FONT_SIZE = 30
+const MIN_FONT_SIZE = 7
+
 export default class WordCloud extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            
+            artistsStreams: []
         }
+        this.wordCloudDivRef = React.createRef()
+    }
+
+    static getDerivedStateFromProps(props, state) {
+        let accStreams = props.tracks.reduce((acc, t) => {
+            if (!acc[t.Artist])
+                acc[t.Artist] = +t.Streams
+            else
+                acc[t.Artist] += +t.Streams
+            return acc
+        }, {})
+        let artistsStreams = []
+        for (const key in accStreams)
+            if (accStreams.hasOwnProperty(key))
+                artistsStreams.push({ artist: key, streams: accStreams[key] })
+        artistsStreams = artistsStreams.sort((a1, a2) => a2.streams - a1.streams)
+        let max = artistsStreams[0].streams
+        let min = artistsStreams[artistsStreams.length - 1].streams
+        let streamsDiff = max - min
+        let fontSizeDiff = MAX_FONT_SIZE - MIN_FONT_SIZE
+        artistsStreams = artistsStreams.map(a => {
+            let dif = a.streams - min
+            let percentage = dif / streamsDiff
+            a.streams = fontSizeDiff * percentage + MIN_FONT_SIZE
+            return a
+        })
+        console.log(artistsStreams)
+        return { artistsStreams: artistsStreams }
+    }
+
+    shouldComponentUpdate(nextProps) {
+        if (nextProps.tracks === this.props.tracks
+            && nextProps.height === this.props.height)
+            return false
+        return true
+    }
+
+    componentDidMount() {
+        this.addD3()
+    }
+
+    componentDidUpdate() {
+        this.addD3()
+    }
+
+    addD3() {
+        d3WordCloud('#word-cloud', this.state.artistsStreams, {
+            w: this.wordCloudDivRef.current.offsetWidth,
+            h: this.props.height - 15,
+            margin: { top: 0, right: 0, bottom: 0, left: 0 },
+            textAttribute: 'artist',
+            sizeAttribute: 'streams',
+            wordsPadding: 3
+        })
     }
 
     render() {
         return (
-            <h1>5</h1>
+            <>
+                <div id="word-cloud" ref={this.wordCloudDivRef}></div>
+            </>
         )
     }
 }
