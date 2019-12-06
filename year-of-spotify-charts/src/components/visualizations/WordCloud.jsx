0976521a46d8895464e022acf60880abfbb1a380
Martim Bravo <martim@bravo.com.pt>
2019-12-06 17:31:52 +0000

    Added sizeScale

--- year-of-spotify-charts/src/components/visualizations/WordCloud.jsx
+++ year-of-spotify-charts/src/components/visualizations/WordCloud.jsx
@@ -27,16 +27,6 @@ export default class WordCloud extends Component {
             if (accStreams.hasOwnProperty(key))
                 artistsStreams.push({ artist: key, streams: accStreams[key] })
         artistsStreams = artistsStreams.sort((a1, a2) => a2.streams - a1.streams)
-        let max = artistsStreams[0].streams
-        let min = artistsStreams[artistsStreams.length - 1].streams
-        let streamsDiff = max - min
-        let fontSizeDiff = MAX_FONT_SIZE - MIN_FONT_SIZE
-        artistsStreams = artistsStreams.map(a => {
-            let dif = a.streams - min
-            let percentage = dif / streamsDiff
-            a.streams = fontSizeDiff * percentage + MIN_FONT_SIZE
-            return a
-        })
         return { artistsStreams: artistsStreams }
     }
 
@@ -56,12 +46,24 @@ export default class WordCloud extends Component {
     }
 
     addD3() {
+        let artistsStreams = this.state.artistsStreams
+        let max = artistsStreams[0].streams
+        let min = artistsStreams[artistsStreams.length - 1].streams
+        let streamsDiff = max - min
+        let fontSizeDiff = MAX_FONT_SIZE - MIN_FONT_SIZE
+
         d3WordCloud('#word-cloud', this.state.artistsStreams, {
             w: this.wordCloudDivRef.current.offsetWidth,
             h: this.props.height - 15,
             margin: { top: 0, right: 0, bottom: 0, left: 0 },
             textAttribute: 'artist',
             valueAttribute: 'streams',
+            sizeScale: v => {
+                let dif = v - min
+                let percentage = dif / streamsDiff
+                v = fontSizeDiff * percentage + MIN_FONT_SIZE
+                return v
+            },
             wordsPadding: 3
         })
     }
