3eb0b5ecbaa30dfe0d5de1245db28e9b44b98bc3
goncaloadolfo <goncaloadolfo20@gmail.com>
2019-12-17 18:55:50 +0000

    build version

---
+++ checkpoint5/build/precache-manifest.7dfafeb57da3ce9b3d74c0889a83ae8e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1eeb593cb447f10f2e8073e08e26a452",
+    "url": "./index.html"
+  },
+  {
+    "revision": "9600291d0fd5114a5be2",
+    "url": "./static/css/main.83007e2c.chunk.css"
+  },
+  {
+    "revision": "a322feb856a5dad5c37d",
+    "url": "./static/js/2.c412575d.chunk.js"
+  },
+  {
+    "revision": "9600291d0fd5114a5be2",
+    "url": "./static/js/main.98f94b9a.chunk.js"
+  },
+  {
+    "revision": "10c4ba876c7d835777c4",
+    "url": "./static/js/runtime-main.1b8a0efd.js"
+  }
+]);
